4bb8f6b79b6e6b23dc3f056e51d8500f281bc442
Grant Hendricks <gtnhen@gmail.com>
2025-08-13 14:32:14 -0400

    Map View uses User Loc 3

--- src/components/RecenterMap.tsx
+++ src/components/RecenterMap.tsx
@@ -4,6 +4,7 @@ import { useMap } from "react-leaflet";
 export function RecenterMap({ center }: { center: [number, number] }) {
     const map = useMap();
     useEffect(() => {
+        console.log('RecenterMap center:', center);
         map.setView(center);
     }, [center, map]);
     return null;
