d2d9b88fa62ed7c6fe60b3169fbda2537812ba2a
Grant Hendricks <gtnhen@gmail.com>
2025-08-13 22:47:50 -0400

    test the logs

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log(import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
